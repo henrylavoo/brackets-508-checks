bf8de668ce134e70913a30be0d51b73cb600715e
tcuongtran <tcuongtran@me.com>
2014-09-12 02:05:33 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+define(function(require, exports, module){
+    var CommandManager = brackets.getModule("command/CommandManager"),
+        EditorManager   = brackets.getModule("editor/EditorManager"),
+        Menus = brackets.getModule("command/Menus"),
+        AppInit = brackets.getModule("utils/AppInit");
+    
+    function check508() {
+        var editor = EditorManager.getFocusedEditor(),
+            text = editor.getSelectedText(),
+            document = editor.document;
+    }
+    
+    AppInit.appReady(function(){
+        
+        var execute_app = '508-chekcs.execute';
+        CommandManager.register('Run 508 Checks', execute_app, check508);
+        var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+        menu.addMenuItem(execute_app);
+    });
+});
